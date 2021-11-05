9bfc7d46d3fc727a0996f079d09bd91ccfdb7dbe
Camilo A <ing_camiloacg@hotmail.com>
2021-11-04 19:01:03 -0500

    Agrega vinculo entre activities con paso de parámetros

--- app/src/main/java/com/misiontic/holamundo04/LoginActivity.java
+++ app/src/main/java/com/misiontic/holamundo04/LoginActivity.java
@@ -2,13 +2,30 @@ package com.misiontic.holamundo04;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText etName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void goToMain(View view) {
+        etName = findViewById(R.id.etPersonName);
+        String name = etName.getText().toString();
+
+        Intent intentMain = new Intent(this, MainActivity.class);
+
+        intentMain.putExtra("user", name);
+
+        startActivity(intentMain);
+    }
+
 }
