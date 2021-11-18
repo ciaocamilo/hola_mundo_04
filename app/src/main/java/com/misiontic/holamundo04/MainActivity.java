38be2cf4be33e8de6bdad36e527ab805c14a6836
Camilo A <ing_camiloacg@hotmail.com>
2021-11-18 18:19:13 -0500

    Se crea menú

--- app/src/main/java/com/misiontic/holamundo04/MainActivity.java
+++ app/src/main/java/com/misiontic/holamundo04/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -19,7 +21,31 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "Bienvenido(a) " + name, Toast.LENGTH_LONG).show();
     }
 
-    public void goToLogin(View view) {
+
+    // Método para generar el menú
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    // Método para asignar funciones al menú
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.item_calculadora) {
+            goToCalculator();
+        } else if (id == R.id.item_contacto) {
+            goToContact();
+        } else if (id == R.id.item_formulario) {
+            goToPersonForm();
+        } else if (id == R.id.item_pedidio) {
+            goToOrders();
+        } else if (id == R.id.item_salir) {
+            goToLogin();
+        }
+        return  super.onOptionsItemSelected(item);
+    }
+
+    public void goToLogin() {
         Intent intentLogin = new Intent(this, LoginActivity.class);
         startActivity(intentLogin);
     }
@@ -29,17 +55,22 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intentCalculator);
     }
 
-    public void goToPersonForm(View view) {
+    public void goToCalculator() {
+        Intent intentCalculator = new Intent(this, CalculatorActivity.class);
+        startActivity(intentCalculator);
+    }
+
+    public void goToPersonForm() {
         Intent intentPersonForm = new Intent(this, PersonFormActivity.class);
         startActivity(intentPersonForm);
     }
 
-    public void goToContact(View view) {
+    public void goToContact() {
         Intent intentContact = new Intent(getApplicationContext(), ContactActivity.class);
         startActivity(intentContact);
     }
 
-    public void goToOrders(View view) {
+    public void goToOrders() {
         Intent intentOrder = new Intent(getApplicationContext(), OrderActivity.class);
         startActivity(intentOrder);
     }
