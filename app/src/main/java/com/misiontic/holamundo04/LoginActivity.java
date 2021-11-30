c78d03d3c1378c66ee0d5d648b88d3769d15b468
Camilo A <ing_camiloacg@hotmail.com>
2021-11-29 20:51:24 -0500

    Crea registro de usuarios en Firebase

--- app/src/main/java/com/misiontic/holamundo04/LoginActivity.java
+++ app/src/main/java/com/misiontic/holamundo04/LoginActivity.java
@@ -8,6 +8,14 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.misiontic.holamundo04.model.Usuario;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -37,12 +45,35 @@ public class LoginActivity extends AppCompatActivity {
         editor.commit();
         //
 
+        pruebaLectura(name);
         startActivity(intentMain);
     }
 
     public void goToNewUser(View view) {
         Intent newUserIntent = new Intent(this, NewUserActivity.class);
         startActivity(newUserIntent);
+    }
+
+    public void pruebaLectura(String nombre) { //FB
+        // Read from the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("usuarios");
+
+        myRef.child("u_" + nombre).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                Usuario value = dataSnapshot.getValue(Usuario.class);
+                Toast.makeText(LoginActivity.this, "Value is: " + value.getNombre(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Toast.makeText(LoginActivity.this, "Failed to read value." + error.toException(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
